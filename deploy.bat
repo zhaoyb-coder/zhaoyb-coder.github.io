bac7250dbbc52c7e6aa3d4183d20fb9bf458b0c0
zhaoyb-coder <18317773990@163.com>
2023-12-04 10:06:36 +0800

    '增加README'

--- deploy.bat
+++ deploy.bat
@@ -1,17 +1,17 @@
-#1、跳转到blog源文件目录，进行npm构建
+:: 1、跳转到blog源文件目录，进行npm构建
 
 cd ..\blog
 call npm run build
 echo(===================== npm build success =====================)
 
-#2、删除旧数据,并复制新数据
+:: 2、删除旧数据,并复制新数据
 cd ..\zhaoyb-coder.github.io
 rd /s /q docs
 md docs
 xcopy /s /e  ..\blog\docs\.vuepress\dist docs
 echo(===================== dir copy success =====================)
 
-#3、提交git
+:: 3、提交git
 git add .
 git commit -m '%1'
 git push
