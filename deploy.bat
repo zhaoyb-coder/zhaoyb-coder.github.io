ecbcbe77e92936167deef21c1fd2aec6429ea7fd
zhaoyb-coder <18317773990@163.com>
2023-12-04 16:11:52 +0800

    提交k8s安装技术总结

--- deploy.bat
+++ deploy.bat
@@ -1,17 +1,22 @@
 :: 1、跳转到blog源文件目录，进行npm构建
-
 cd ..\blog
 call npm run build
 echo(===================== npm build success =====================)
 
-:: 2、删除旧数据,并复制新数据
+:: 2、提交blog仓库到github
+git add .
+git commit -m %1
+git push
+echo(===================== git push success =====================)
+
+:: 3、删除旧数据,并复制新数据
 cd ..\zhaoyb-coder.github.io
 rd /s /q docs
 md docs
 xcopy /s /e  ..\blog\docs\.vuepress\dist docs
 echo(===================== dir copy success =====================)
 
-:: 3、提交git
+:: 4、提交git
 git add .
 git commit -m %1
 git push
